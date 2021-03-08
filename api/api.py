f4142edb0fbbbdbf429747b8034689da2a9d66b8
elias <elias.poylio@gmail.com>
2021-03-08 21:07:04 +0200

    dev: remove test endpoint

--- api/api.py
+++ api/api.py
@@ -4,10 +4,6 @@ import numpy as np
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
-
 @app.route('/sigmoid/<x>')
 def sigmoid(x):
     nr = int(x)
