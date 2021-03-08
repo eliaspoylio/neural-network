7ae59ae76b06d35b283cc86c7da10a0c1a8cdfef
elias <elias.poylio@gmail.com>
2021-03-08 21:05:04 +0200

    dev: add sigmoid

--- api/api.py
+++ api/api.py
@@ -1,8 +1,15 @@
 import time
 from flask import Flask
+import numpy as np
 
 app = Flask(__name__)
 
 @app.route('/time')
 def get_current_time():
     return {'time': time.time()}
+
+@app.route('/sigmoid/<x>')
+def sigmoid(x):
+    nr = int(x)
+    sigmoid = 1 / (1 * np.exp(-nr))
+    return { 'sigmoid': sigmoid }
