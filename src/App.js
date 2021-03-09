337945a364d9300d544a574d0b683272e967d9d5
elias <elias.poylio@gmail.com>
2021-03-09 22:55:46 +0200

    ui: training io

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -14,20 +13,43 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>Test sigmoid is {sigmoid}.</p>
-      </header>
+      <div className="wrapper">
+      <h1>Training inputs</h1>
+      <form>
+      <fieldset>
+           <input name="0_0" value="0" />
+           <input name="0_1" value="0" />
+           <input name="0_2" value="1" />
+       </fieldset>
+       <fieldset>
+           <input name="1_0" value="1"/>
+           <input name="1_1" value="1"/>
+           <input name="1_2" value="1"/>
+       </fieldset>
+       <fieldset>
+           <input name="2_0" value="1"/>
+           <input name="2_1" value="0"/>
+           <input name="2_2" value="1"/>
+       </fieldset>
+       <fieldset>
+           <input name="3_0" value="0"/>
+           <input name="3_1" value="1"/>
+           <input name="3_2" value="1"/>
+       </fieldset>
+       <h1>Training outputs</h1>
+           <input name="o0" value="0"/>
+           <br></br>
+           <input name="o1" value="1"/>
+           <br></br>
+           <input name="o2" value="0"/>
+           <br></br>
+           <input name="o3" value="0"/>
+           <br></br>
+       <button type="submit">Submit</button>
+      </form>
+    </div>
+    </header>
     </div>
   );
 }
